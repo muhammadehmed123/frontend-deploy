54ca53f1377fe860520098ab20dff237e5f49259
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-02 13:06:21 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+  // /** @type {import('next').NextConfig} */
+  // const nextConfig = {
+  //   reactStrictMode: true,
+  // }
+
+  // module.exports = nextConfig
+
+  module.exports={
+    images:{
+      domains:['localhost','app.skillbuilder.online','app.recruitinn.ai']
+    }
+  }
